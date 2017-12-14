c3c7f081678c593023575290af1f0384a6e1612f
AlexanderPavlenkoFTC <alexandr.pavlenko14@gmail.com>
2017-12-14 11:33:14 +0200

    init commit

---
+++ videoStore/src/main/java/com/ua/videoStore/VideoStoreApplication.java
@@ -0,0 +1,12 @@
+package com.ua.videoStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoStoreApplication.class, args);
+	}
+}
